c81e404a9c0f7d44ae738110fddecbc7ea6cd20a
Biniam <biniam22@gmail.com>
2017-10-02 18:17:51 -0400

    first commit

---
+++ bni.py
@@ -0,0 +1,12 @@
+print 'hello'
+# Get even 1000
+for i in range (1, 1001):
+    if i % 2 == 0:
+        print i;
+
+# Sum odd 5000
+sum = 0
+for i in range (1,5001):
+    if i % 2 ==1 :
+        sum= sum +1
+        print sum
